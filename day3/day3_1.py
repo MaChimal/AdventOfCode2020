131c54a3389ba8f610a7f1881277aef695f291c2
MaChimal <mariochimal00aa@gmail.com>
2020-12-03 18:51:50 -0600

    day3

---
+++ day3/day3_1.py
@@ -0,0 +1,26 @@
+### Day 23: Toboggan Trajectory ###
+
+## Part 1
+
+answer = 0
+
+map = []
+
+for _x in open("day3.txt"):
+    _x = _x.strip()
+    map.append(_x)
+
+row = 0
+col = 0
+
+m = len(map)
+
+while row+1 < m:
+    row += 1
+    col += 3
+
+    position = map[row][col % len(map[row])]
+    if position == "#":
+        answer += 1
+
+print(answer)
