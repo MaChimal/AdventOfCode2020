c2c26a5b498b92c2b1ba53946ee83082f78d2913
MaChimal <mariochimal00aa@gmail.com>
2020-12-06 01:33:38 -0600

    day6

--- day6/day6_2.py
+++ day6/day6_2.py
@@ -4,7 +4,7 @@
 
 answer = 0
 
-for x in open("day66.txt").read().split("\n\n"):
+for x in open("day6.txt").read().split("\n\n"):
     letters = set.intersection(*[set(s) for s in x.split()])
     answer += len(letters)
 
